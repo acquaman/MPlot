f4c44613ed6ff65964c68896197b6733cdef1695
Mark Boots <mark.boots@usask.ca>
2011-05-25 20:14:04 -0600

    optimizes MPlotColorMap to share standard color maps with 256-resolution system-wide. This means that color values only need to be computed once for each standard color map.

--- src/MPlot/MPlotColorMap.cpp
+++ src/MPlot/MPlotColorMap.cpp
@@ -4,18 +4,22 @@
 
 #include "MPlotColorMap.h"
 
+/// System-wide pre-computed values for default color maps: optimizes the creation of new default color maps. These all have a standard resolution of 256.
+QVector<QVector<QRgb>*> MPlotColorMap::precomputedMaps_ = QVector<QVector<QRgb>*>(13,0);
 
 /// Constructs a default color map (Corresponding to MPlotColorMap::Jet)
 MPlotColorMap::MPlotColorMap(int resolution)
 	: colorArray_(resolution)
 {
 	blendMode_ = RGB;
+	standardColorMapValue_ = Jet;
 	colorStops_ << QGradientStop(0.0, QColor(0, 0, 131))
-			<< QGradientStop(0.121569, QColor(0, 0, 255))
-			<< QGradientStop(0.372549, QColor(0, 255, 255))
-			<< QGradientStop(0.623529, QColor(255, 255, 0))
-			<< QGradientStop(0.874510, QColor(255, 0, 0))
-			<< QGradientStop(1.0, QColor(128, 0, 0));
+				<< QGradientStop(0.121569, QColor(0, 0, 255))
+				<< QGradientStop(0.372549, QColor(0, 255, 255))
+				<< QGradientStop(0.623529, QColor(255, 255, 0))
+				<< QGradientStop(0.874510, QColor(255, 0, 0))
+				<< QGradientStop(1.0, QColor(128, 0, 0));
+
 	recomputeCachedColorsRequired_ = true;
 }
 
@@ -24,6 +28,7 @@ MPlotColorMap::MPlotColorMap(const QColor& color1, const QColor& color2, int res
 	: colorArray_(resolution)
 {
 	blendMode_ = RGB;
+	standardColorMapValue_ = -1;
 	colorStops_ << QGradientStop(0.0, color1) << QGradientStop(1.0, color2);
 	recomputeCachedColorsRequired_ = true;
 }
@@ -33,6 +38,7 @@ MPlotColorMap::MPlotColorMap(const QGradientStops& colorStops, int resolution)
 	: colorArray_(resolution), colorStops_(colorStops)
 {
 	blendMode_ = RGB;
+	standardColorMapValue_ = -1;
 	recomputeCachedColorsRequired_ = true;
 }
 
@@ -41,82 +47,93 @@ MPlotColorMap::MPlotColorMap(StandardColorMap colorMap, int resolution)
 	: colorArray_(resolution)
 {
 	blendMode_ = RGB;
+	standardColorMapValue_ = colorMap;
 
 	switch(colorMap){
 
 	case Autumn:
 		colorStops_ << QGradientStop(0, QColor(255, 0, 0))
-				<< QGradientStop(1, QColor(255, 255, 0));
+					<< QGradientStop(1, QColor(255, 255, 0));
 		break;
 	case Bone:
 		colorStops_ << QGradientStop(0, QColor(0, 0, 0))
-				<< QGradientStop(0.372549, QColor(83, 83, 115))
-				<< QGradientStop(0.749020, QColor(167, 199, 199))
-				<< QGradientStop(1, QColor(255, 255, 255));
+					<< QGradientStop(0.372549, QColor(83, 83, 115))
+					<< QGradientStop(0.749020, QColor(167, 199, 199))
+					<< QGradientStop(1, QColor(255, 255, 255));
 		break;
 	case Cool:
 		colorStops_ << QGradientStop(0, QColor(0, 255, 255))
-				<< QGradientStop(1, QColor(255, 0, 255));
+					<< QGradientStop(1, QColor(255, 0, 255));
 		break;
 	case Copper:
 		colorStops_ << QGradientStop(0, QColor(0, 0, 0))
-				<< QGradientStop(1, QColor(255, 199, 127));
+					<< QGradientStop(1, QColor(255, 199, 127));
 		break;
 	case Gray:
 		colorStops_ << QGradientStop(0, QColor(0, 0, 0))
-				<< QGradientStop(1, QColor(255, 255, 255));
+					<< QGradientStop(1, QColor(255, 255, 255));
 		break;
 	case Hot:
 		colorStops_ << QGradientStop(0, QColor(3, 0, 0))
-				<< QGradientStop(0.372549, QColor(255, 0, 0))
-				<< QGradientStop(0.749020, QColor(255, 255, 0))
-				<< QGradientStop(1, QColor(255, 255, 255));
+					<< QGradientStop(0.372549, QColor(255, 0, 0))
+					<< QGradientStop(0.749020, QColor(255, 255, 0))
+					<< QGradientStop(1, QColor(255, 255, 255));
 		break;
 	case Hsv:
 		colorStops_ << QGradientStop(0, QColor(255, 0, 0))
-				<< QGradientStop(0.4, QColor(0, 255, 99))
-				<< QGradientStop(0.8, QColor(199, 0, 255))
-				<< QGradientStop(1, QColor(255, 0, 6));
+					<< QGradientStop(0.4, QColor(0, 255, 99))
+					<< QGradientStop(0.8, QColor(199, 0, 255))
+					<< QGradientStop(1, QColor(255, 0, 6));
 		setBlendMode(HSV);
 		break;
-	case Jet:
-		colorStops_ << QGradientStop(0.0, QColor(0, 0, 131))
-				<< QGradientStop(0.121569, QColor(0, 0, 255))
-				<< QGradientStop(0.372549, QColor(0, 255, 255))
-				<< QGradientStop(0.623529, QColor(255, 255, 0))
-				<< QGradientStop(0.874510, QColor(255, 0, 0))
-				<< QGradientStop(1.0, QColor(128, 0, 0));
-		break;
+
 	case Pink:// This is a linear interpolation of a non-linear calculation.
 		colorStops_ << QGradientStop(0, QColor(15, 0, 0))
-				<< QGradientStop(0.372549, QColor(195, 128, 128))
-				<< QGradientStop(0.749020, QColor(234, 234, 181))
-				<< QGradientStop(1, QColor(255, 255, 255));
+					<< QGradientStop(0.372549, QColor(195, 128, 128))
+					<< QGradientStop(0.749020, QColor(234, 234, 181))
+					<< QGradientStop(1, QColor(255, 255, 255));
 		break;
+
 	case Spring:
 		colorStops_ << QGradientStop(0, QColor(255, 0, 255))
-				<< QGradientStop(1, QColor(255, 255, 0));
+					<< QGradientStop(1, QColor(255, 255, 0));
 		break;
+
 	case Summer:
 		colorStops_ << QGradientStop(0, QColor(0, 128, 102))
-				<< QGradientStop(1, QColor(255, 255, 102));
+					<< QGradientStop(1, QColor(255, 255, 102));
 		break;
+
 	case White:
 		colorStops_ << QGradientStop(0, QColor(255, 255, 255))
-				<< QGradientStop(1, QColor(255, 255, 255));
+					<< QGradientStop(1, QColor(255, 255, 255));
 		break;
+
 	case Winter:
 		colorStops_ << QGradientStop(0, QColor(0, 0, 255))
-				<< QGradientStop(1, QColor(0, 255, 128));
+					<< QGradientStop(1, QColor(0, 255, 128));
+		break;
+
+	default:
+		standardColorMapValue_ = Jet;
+	case Jet:
+		colorStops_ << QGradientStop(0.0, QColor(0, 0, 131))
+					<< QGradientStop(0.121569, QColor(0, 0, 255))
+					<< QGradientStop(0.372549, QColor(0, 255, 255))
+					<< QGradientStop(0.623529, QColor(255, 255, 0))
+					<< QGradientStop(0.874510, QColor(255, 0, 0))
+					<< QGradientStop(1.0, QColor(128, 0, 0));
 		break;
 	}
 
 	recomputeCachedColorsRequired_ = true;
 }
 
+
 /// Replaces the current set of stop points with the given \c stopPoints. The positions of the points must be in the range 0 to 1, and must be sorted with the lowest point first.
 void MPlotColorMap::setStops(const QGradientStops& stopPoints)
 {
+	standardColorMapValue_ = -1;
 	colorStops_ = stopPoints;
 	recomputeCachedColorsRequired_ = true;
 }
@@ -124,6 +141,7 @@ void MPlotColorMap::setStops(const QGradientStops& stopPoints)
 /// Adds a stop the given \c position with the color \c color.  Note that position must be between 0 and 1.
 void MPlotColorMap::addStopAt(qreal position, const QColor& color)
 {
+	standardColorMapValue_ = -1;
 	for (int i = 0; i < colorStops_.size(); i++){
 
 		// The first time position is smaller than the current position, put it in that place.  Exit loop.
@@ -140,6 +158,15 @@ void MPlotColorMap::addStopAt(qreal position, const QColor& color)
 /// Helper function to recompute the cached color array when the color stops, resolution, or blend mode are changed.
 void MPlotColorMap::recomputeCachedColors() const
 {
+	// we're going to be recomputing the colorArray_, so reset this flag.
+	recomputeCachedColorsRequired_ = false;
+
+	if(resolution() == 256 &&
+			standardColorMapValue_ != -1 &&
+			precomputedMaps_.at(standardColorMapValue_) ) {
+		colorArray_ = *(precomputedMaps_.at(standardColorMapValue_));
+		return;
+	}
 
 	// If no stops were given, produce a generic grayscale colour map.
 	if (colorStops_.isEmpty()){
@@ -184,17 +211,17 @@ void MPlotColorMap::recomputeCachedColors() const
 			if (blendMode() == HSV)
 				for (int i = 0; i <= endMinusStart; i++)
 					colorArray_[startIndex+i] = QColor::fromHsv(start.second.hue()+(end.second.hue()-start.second.hue())*i/endMinusStart,
-													  start.second.saturation()+(end.second.saturation()-start.second.saturation())*i/endMinusStart,
-													  start.second.value()+(end.second.value()-start.second.value())*i/endMinusStart,
-													  start.second.alpha()+(end.second.alpha()-start.second.alpha())*i/endMinusStart)
-													  .rgb();
+																start.second.saturation()+(end.second.saturation()-start.second.saturation())*i/endMinusStart,
+																start.second.value()+(end.second.value()-start.second.value())*i/endMinusStart,
+																start.second.alpha()+(end.second.alpha()-start.second.alpha())*i/endMinusStart)
+							.rgb();
 			else if (blendMode() == RGB)
 				for (int i = 0; i <= endMinusStart; i++)
 					colorArray_[startIndex+i] = QColor::fromRgb(start.second.red()+(end.second.red()-start.second.red())*i/endMinusStart,
-													  start.second.green()+(end.second.green()-start.second.green())*i/endMinusStart,
-													  start.second.blue()+(end.second.blue()-start.second.blue())*i/endMinusStart,
-													  start.second.alpha()+(end.second.alpha()-start.second.alpha())*i/endMinusStart)
-													  .rgb();
+																start.second.green()+(end.second.green()-start.second.green())*i/endMinusStart,
+																start.second.blue()+(end.second.blue()-start.second.blue())*i/endMinusStart,
+																start.second.alpha()+(end.second.alpha()-start.second.alpha())*i/endMinusStart)
+							.rgb();
 		}
 
 		// If the last stop isn't at 1 then fill the rest of the colour map with the last stop's colour.
@@ -203,42 +230,18 @@ void MPlotColorMap::recomputeCachedColors() const
 				colorArray_[i] = colorStops_.last().second.rgb();
 	}
 
-	// we're done recomputing the colorArray_, so reset this flag.
-	recomputeCachedColorsRequired_ = false;
+
+
+	// system-wide optimization: sharing standard color maps. (Valid only for default resolution of 256)
+	// Here we share the map we've just computed, if it hasn't been shared already.
+	if(resolution() == 256 &&
+			standardColorMapValue_ != -1 &&
+			precomputedMaps_.at(standardColorMapValue_) == 0) {
+
+		precomputedMaps_[standardColorMapValue_] = new QVector<QRgb>();
+		*(precomputedMaps_[standardColorMapValue_]) = colorArray_;
+	}
 }
-
-/*
-/// Constructs a color map with \c start and \c end values.
-MPlotLinearColorMap::MPlotLinearColorMap(const QColor& start, const QColor& finish)
-	: start_(start), finish_(finish)
-{
-}
-
-///  return a QRgb (unsigned int) representing the color for a given \c value within a \c range. (Faster than returning a full QColor)
-QRgb MPlotLinearColorMap::rgb(qreal value, MPlotInterval range) const {
-
-	qreal Ratio = (value - range.first)/(range.second - range.first);
-	QRgb Col1 = start_.rgba();
-	QRgb Col2 = finish_.rgba();
-
-	int A1=qAlpha(Col1), R1=qRed(Col1), G1=qGreen(Col1), B1=qBlue(Col1);
-	int A2=qAlpha(Col2), R2=qRed(Col2), G2=qGreen(Col2), B2=qBlue(Col2);
-	//int A2=(Col2>>24)&0xFF, R2=(Col2>>16)&0xFF, G2=(Col2>>8)&0xFF, B2=Col2&0xFF;
-	int Color;
-	if (Ratio<=0) return Col1;	// Ratio parameter must be between 0 and 1
-	else if (Ratio>=1) return Col2;
-	Color=	(int)(A1+(A2-A1)*Ratio+0.5)<<24 |
-			(int)(R1+(R2-R1)*Ratio+0.5)<<16 |		// rounding
-			(int)(G1+(G2-G1)*Ratio+0.5)<<8  |
-			(int)(B1+(B2-B1)*Ratio+0.5);
-	return Color;
-
-}
-/// return the color representing a given \c value within a \c range.
-QColor MPlotLinearColorMap::color(qreal value, MPlotInterval range) const {
-	return QColor::fromRgba(rgb(value, range));
-}
-*/
 
 
 
